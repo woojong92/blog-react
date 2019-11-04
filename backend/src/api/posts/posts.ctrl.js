bbac67d38871ffb1203cd339d56f40104ceafd6e
woojong92 <woojong92@gmail.com>
2019-11-04 22:52:35 +0900

    mongodb를 이용한 REST API 구현

--- backend/src/api/posts/posts.ctrl.js
+++ backend/src/api/posts/posts.ctrl.js
@@ -1,129 +1,51 @@
-let postId = 1 // id의 초기값
+import Post from '../../models/post';
 
-// posts 배열 초기 데이터
-const posts = [
-    {
-        id: 1,
-        title: '제목',
-        body: '내용',
+// 포스트의 인스턴스를 만들 때는, new 키워드를 사용
+// 그리고 생성자 함수의 파라미터에 정보를 지닌 객체를 넣는다. 
+// save() 함수를 실행시켜야 데이터베이스에 저장됨.
+export const write = async ctx => {
+    const {title, body, tags} = ctx.request.body;
+    const post = new Post({
+        title,
+        body,
+        tags,
+    });
+    try{
+        await post.save();
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
     }
-]
-
-/* 포스트 작성
-POST /api/posts
-{ title, body }
-*/
-export const write = ctx => {
-    // REST API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
-    const { title, body } = ctx.request.body
-    postId += 1
-    const post = { id: postId, title, body }
-    posts.push(post)
-    ctx.body = post
 }
 
-/**
- * 포스트 목록 조회
- * GET /api/posts
- */
-export const list = ctx => {
-    ctx.body = posts
+// find() 함수를 호출한 후에 exec()를 붙여 주어야 서버에 쿼리를 요청한다.
+export const list = async ctx => {
+    try{
+        const posts = await Post.find().exec();
+        ctx.body = posts;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
 }
 
-/** 
- * 특적 포스트 조회
- * GET /api/posts/:id
- */
-export const read = ctx => {
-    const { id } = ctx.params
-    // 주어진 id 값으로 포스트를 찾습니다.
-    // 파라미터로 받아 온 값은 문자열 형식으로 파라미터를 숫자로 변환하거나
-    // 비교할 p.id 값을 문자열로 변경해야 합니다.
-    const post = posts.find(p => p.id.toString() === id)
-    // 포스트가 없으면 오류를 반환합니다.
-    if (!post) {
-        ctx.status = 404
-        ctx.body = {
-            message: '포스트가 존재하지 않습니다.'
+export const read = async ctx => {
+    const {id} = ctx.params;
+    try{
+        const post = await Post.findById(id).exec();
+        if(!post){
+            ctx.status = 404; // not found
+            return;
         }
-        return 
+        ctx.body = post;
+    }catch(e) {
+        ctx.throw(500, e)
     }
-    ctx.body = post
 }
 
-/**
- * 특정 포스트 제거
- * DELETE /api/posts/:id
- */
-export const remove = ctx => {
-    const { id } = ctx.params
-    // 해당 id를 가진 post가 몇 번째인지 확인합니다.
-    const index = posts.findIndex(p => p.id.toString() === id)
-    // 포스트가 없으면 오류를 반환합니다.
-    if ( index === -1 ) {
-        ctx.status = 404
-        ctx.body = {
-            message: '포스트가 존재하지 않습니다.',
-        }
-        return
-    }
-    // index번째 아이템을 제거합니다.
-    posts.splice(index, 1)
-    ctx.status = 204 // No Content
-}
+export const remove = ctx => {}
 
-/**
- * 포스트 수정(교체)
- * PUT /api/posts/:id
- * { title, body }
- */
-export const replace = ctx => {
-    // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용
-    const { id } = ctx.params
-    // 해당 id를 가진 post가 몇번째인지 확인
-    const index = posts.findIndex(p => p.id.toString() === id)
-    // 포스트가 없으면 오류를 반환
-    if( index === -1 ) {
-        ctx.status = 404 
-        ctx.body = {
-            message: '포스터가 존재하지 않습니다.'
-        }
-        return
-    }
-    // 전체 객체를 덮어 씌운다.
-    // 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만듬.
-    posts[index] = {
-        id,
-        ...ctx.request.body
-    }
-    ctx.body = posts[index]
-}
+export const update = ctx => {}
 
-/**
- * 포스트 수정(특정 필드 변경)
- * PATCH /api/posts/:id
- * { title, body }
- */
-export const update = ctx => {
-    // PATCH 메서드는 주어진 필드만 교체합니다.
-    const { id } = ctx.params
-    // 해당 id를 가진 post가 몇 번째인지 확인
-    const index = posts.findIndex( p => p.id.toString() === id ) 
-    // 포스트가 없으면 오류를 반환
-    if( index === -1 ) {
-        ctx.status = 404
-        ctx.body = { 
-            message: '포스트가 존재하지 않습니다.'
-        }
-        return
-    }
-    // 기존 값에 정보를 덮어 씌운다.
-    posts[index] = {
-        ...posts[index],
-        ...ctx.request.body,
-    }
-    ctx.body = posts[index]
-}
 
 
 
--- backend/src/models/post.js
+++ backend/src/models/post.js
@@ -11,6 +11,8 @@ const PostSchema = new Schema({
         default: Date.now,
     }
 })
-
+// mongoose.model은 두 개의 파라미터를 필요함.
+// 첫번째 파라미터는 스키마의 이름,
+// 두번째 파라미터는 스키마 객체
 const Post = mongoose.model('Post', PostSchema)
 export default Post
