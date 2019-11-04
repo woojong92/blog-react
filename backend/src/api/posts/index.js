ebd959a8bd324a01703710cece905bcb766bc978
woojong92 <woojong92@gmail.com>
2019-11-04 23:20:43 +0900

    update

--- backend/src/api/posts/index.js
+++ backend/src/api/posts/index.js
@@ -1,18 +1,11 @@
-const Router = require('koa-router')
-const postsCtrl = require('./posts.ctrl')
+import Router from 'koa-router'
+import * as postsCtrl from './posts.ctrl'
 
 const posts = new Router()
 
-const printInfo = ctx => {
-    ctx.body = {
-        method: ctx.method,
-        path: ctx.path,
-        params: ctx.params,
-    }
-}
-
 posts.get('/', postsCtrl.list)
 posts.post('/', postsCtrl.write)
+
 posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read)
 posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove)
 posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update)
