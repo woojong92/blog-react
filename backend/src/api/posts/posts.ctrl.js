b920d32c174a146cf0220af24087bc682a0d19f3
woojong92 <woojong92@gmail.com>
2019-11-04 23:03:02 +0900

    ObjectId 검증

--- backend/src/api/posts/posts.ctrl.js
+++ backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,20 @@
 import Post from '../../models/post';
+import mongoose from 'mongoose';
+
+// 요청 검증
+// 500 오류는 보통 서버에서 처리하지 않아 내부적으로 문제가 생겼을 때 발생
+// 잘못된 id를 전달했다면 400 bad request 오류를 띄워주는 게 맞다. 
+const {ObjectId} = mongoose.Types;
+
+// id 값을 검증하는 미들웨어
+export const checkObjectId = ( ctx, next ) => {
+    const {id} = ctx.params;
+    if( !ObjectId.isValid(id)) {
+        ctx.status = 400; // bad request
+        return;
+    }
+    return next();
+}
 
 // 포스트의 인스턴스를 만들 때는, new 키워드를 사용
 // 그리고 생성자 함수의 파라미터에 정보를 지닌 객체를 넣는다. 
@@ -42,9 +58,36 @@ export const read = async ctx => {
     }
 }
 
-export const remove = ctx => {}
+// remove(): 특정 조건을 만족하는 데이터를 모두 지움
+// findByIdAndRemove(): id를 찾아서 지운다. 
+// findOneAndRemove(): 특정 조건을 만족하는 데이터 하나를 찾아서 제거
+export const remove = async ctx => {
+    const {id} = ctx.params;
+    try{
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204; // No Content ( 성공은 했지만 응답할 데이터는 없음 )
+    }catch(e) {
+        throw(500, e);
+    }
+}
 
-export const update = ctx => {}
+// findByIdAndUpdate() 함수 사용
+export const update = async ctx => {
+    const {id} = ctx.params;
+    try{
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true, // 이 값을 성정하면 업데이트된 데이터를 반환
+                        // false일 때는 업데이트되기 전의 데이터를 반환한다.
+        }).exec();
+        if(!post){
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    }catch(e) {
+        throw(500, e);
+    }
+}
 
 
 
